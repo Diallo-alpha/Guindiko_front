d3b553c1c654b7ba8984f60470288c9f70e9fb45
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-15 12:03:52 +0000

    configration de base projet guindiko avec angular

---
+++ src/app/components/menteur/menteur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menteur',
+  standalone: true,
+  imports: [],
+  templateUrl: './menteur.component.html',
+  styleUrl: './menteur.component.css'
+})
+export class MenteurComponent {
+
+}
