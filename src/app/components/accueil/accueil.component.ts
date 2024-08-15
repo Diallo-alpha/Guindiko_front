a13377b6385ddd7fb5968415444b99c1af613f7b
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-15 17:45:44 +0000

    integration des pages

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,RouterModule, FooterComponent],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.css'
 })
