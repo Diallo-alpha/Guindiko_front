d54480eec62ac5b0722ab5f7663df8834a73035d
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-15 17:52:43 +0000

    correction des routes

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterModule],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
