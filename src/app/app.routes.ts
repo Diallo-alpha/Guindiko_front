2e7530a8129de40aa657996cd54c6679dfde431b
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-15 12:49:49 +0000

    page accueil navbar et deux premiere section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 export const routes: Routes = [
-  {path: 'navbar', component:NavbarComponent}
+  {path: 'navbar', component:NavbarComponent},
+  {path: '', component:AccueilComponent}
 ];
---
+++ src/app/components/accueil/accueil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+@Component({
+  selector: 'app-accueil',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.css'
+})
+export class AccueilComponent {
+
+}
