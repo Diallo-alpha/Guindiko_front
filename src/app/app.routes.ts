52e62541e387125fe4f12c6ceb629b55f7e19c56
Oumyf <falladiaraoumy@gmail.com>
2024-08-20 06:58:01 +0000

    intégration du dahboard de l'admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path: 'navbar', component:NavbarComponent}
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
