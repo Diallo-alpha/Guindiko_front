c32611b1f41625293f52ce4a25d2edde6de0155c
Cheikh <cheikhsane656@gmail.com>
2024-08-26 03:01:03 +0000

    profile-mentor

--- src/app/components/mentor/profil-mentor/profil-mentor.component.ts
+++ src/app/components/mentor/profil-mentor/profil-mentor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
 import { NavbarComponent } from '../../navbar/navbar.component'
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-profil-mentor',
@@ -21,41 +22,46 @@ export class ProfilMentorComponent implements OnInit {
     photoUrl: ''
   }
 
-  private apiUrl = 'https://your-api-endpoint.com/mentor-profile' // Remplacez par l'URL de votre API
-
-  constructor (private http: HttpClient) {}
+  private apiUrl = 'https://your-api-endpoint.com/mentor-profile' 
+  constructor () {}
 
   ngOnInit () {
     this.fetchMentorProfile()
   }
 
-  fetchMentorProfile () {
-    this.http.get(this.apiUrl).subscribe(
-      (data: any) => {
-        this.mentor = data
-      },
-      error => {
-        console.error(
-          'Erreur lors de la récupération des données du mentor',
-          error
-        )
+  async fetchMentorProfile () {
+    try {
+      const response = await fetch(this.apiUrl)
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
       }
-    )
+      const data = await response.json()
+      this.mentor = data
+    } catch (error) {
+      console.error(
+        'Erreur lors de la récupération des données du mentor',
+        error
+      )
+    }
   }
 
   requestMentorship () {
     console.log('Demande de mentorat envoyée')
+    //  la logique pour demander un mentorat
   }
 
   viewArticles () {
     console.log('Voir les articles')
+    //  la logique pour voir les articles
   }
 
   viewSessions () {
     console.log('Voir les sessions')
+    //  la logique pour voir les sessions
   }
 
   viewMembers () {
     console.log('Voir les membres')
+    //  la logique pour voir les membres
   }
 }
